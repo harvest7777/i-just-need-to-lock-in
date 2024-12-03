c788d8065eb65f73e51a3faea53b9431b78bbb10
xiuv <333harvest.moon333@gmail.com>
2024-12-03 11:44:42 -0800

    made simple ui and backend functionality

--- app/protected/todays-tasks/page.tsx
+++ app/protected/todays-tasks/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-
+import AddCompletedTask from "./_components/add_completed_task";
 export default function TodaysTasks() {
     return(
         <>
             <h1>Todays Tasks:</h1>
+            <AddCompletedTask/>
         </>
     )
 }
