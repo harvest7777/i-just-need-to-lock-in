05186dd1edfca220acd56ad9cfaa30fcfe0e11fc
xiuv <333harvest.moon333@gmail.com>
2025-01-13 20:45:19 -0800

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,17 @@ import Link from "next/link";
 import hero_image from "../app/public/images/hero.png"
 export default async function Index() {
   return (
-      <main className="h-full bg-appFg absolute top-0 left-0">
+      <div className="min-h-screen h-max w-full bg-appFg absolute top-0 left-0 pb-10">
         {/* hero container */}
         <div className="flex mt-16 justify-center items-center p-5">
           {/* header */}
-          <div className="flex flex-col items-center w-1/2">
-            <h1 className="font-header text-6xl lg:text-8xl">LOCK IN!!!</h1>
+          <div className="flex flex-col items-center lg:w-1/2 w-full">
+            <h1 className="font-header text-7xl lg:text-8xl">LOCK IN!!!</h1>
             <h2 className="italic">track productivity and stay motivated... with friends!</h2>
 
             {/* button container */}
             <div className="flex space-x-10 mt-5">
               <Link href="sign-up" className="w-32 text-center bg-emerald-600 text-appFg rounded-md font-header p-2 btn-hover">I'M READY TO LOCK IN</Link>
-              <p className="w-32 text-center bg-appBg rounded-md p-2 btn-hover">convince me more</p>
             </div>
           </div>
         
@@ -34,7 +33,7 @@ export default async function Index() {
         </div>
 
         {/* selling points */}
-        <div className="flex w-full space-x-8 justify-center mt-20">
+        <div className="flex md:flex-row flex-col items-center align-middle space-y-8 md:space-y-0 w-full md:space-x-8 justify-center mt-20">
           <div className="w-1/4 flex flex-col items-center">
             <GoStopwatch className="text-4xl"/>
             <h1 className="text-center font-bold">Daily Task Management</h1>
@@ -51,6 +50,6 @@ export default async function Index() {
             <p className="text-center">Connect with friends, compare productivity, and inspire each other</p>
           </div>
         </div>
-      </main>
+      </div>
   );
 }
