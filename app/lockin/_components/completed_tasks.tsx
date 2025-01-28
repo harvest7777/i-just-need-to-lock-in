adcf899dd5b807fd192d94a95fe943c222107138
xiuv <333harvest.moon333@gmail.com>
2025-01-28 08:13:18 -0800

    completd task totasls

--- app/lockin/_components/completed_tasks.tsx
+++ app/lockin/_components/completed_tasks.tsx
@@ -1,5 +1,6 @@
 // CompletedTasks.tsx
-import React from "react";
+import React, { useState } from "react";
+import ChooseCompleted from "./choose_completed_display_dropdown";
 
 interface CompletedTasksProps {
   completedTasks: Task[];
@@ -8,26 +9,27 @@ interface CompletedTasksProps {
 const CompletedTasks: React.FC<CompletedTasksProps> = ({ completedTasks, taskIntervals }) => {
   // Pre-process intervals into a Map
   const intervalMap = processIntervals(taskIntervals);
+  const [timeSpentDisplay, setTimeSpentDisplay] = useState<string>("today");
 
   return (
     <div className="flex flex-col items-center w-full p-2">
       <h1 className="font-bold text-xl pl-2 w-full">Completed Today</h1>
       {completedTasks?.map((task) => {
-        const totalSeconds = intervalMap.get(task.task_id) || 0;
-        const totalMinutes = Math.round(totalSeconds / 60);
-
+        const dailySeconds = intervalMap.get(task.task_id) || 0;
+        const dailyMinutes = Math.round(dailySeconds/ 60);
+        const totalMinutes = Math.round(task.seconds_spent/60)
         return (
           <div className="flex w-full my-1" key={task.task_id}>
             <p className="italic pl-2 w-full line-through text-neutral-400">
               {task.name}
             </p>
-            {/* depracated, no longer showing total minutes here */}
-            {/* <p className="w-1/3 italic text-neutral-400 rounded-r-md rounded-tr-md text-right pr-2">
-              {totalMinutes}m
-            </p> */}
+            {timeSpentDisplay=="today" && (<p className="w-1/3 italic text-neutral-400 rounded-r-md rounded-tr-md text-right pr-2">{dailyMinutes}m</p>)}
+            {timeSpentDisplay=="total" && (<p className="w-1/3 italic text-neutral-400 rounded-r-md rounded-tr-md text-right pr-2">{totalMinutes}m</p>)}
+
           </div>
         );
       })}
+      <ChooseCompleted timeSpentDisplay={timeSpentDisplay} setTimeSpentDisplay={setTimeSpentDisplay}/>
     </div>
   );
 };
