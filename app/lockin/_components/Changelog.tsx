64a112e0e5a34f39fc1a96ad7e3f075b63fae2ff
harvset7777 <333harvest.moon333@gmail.com>
2025-02-23 20:12:48 -0800

    changelog

--- app/lockin/_components/Changelog.tsx
+++ app/lockin/_components/Changelog.tsx
@@ -4,20 +4,21 @@ import { useState } from "react";
 import { IoCloseOutline } from "react-icons/io5";
 
 export default function Changelog() {
-    const [visible, setVisible] = useState<boolean>(true);
-    return(
-        <>
-        {visible&&
+  const [visible, setVisible] = useState<boolean>(true);
+  return (
+    <>
+      {visible &&
         <div className="border-4 h-fit border-neutral-400 border-dashed  outline-neutral-400 p-2 rounded-2xl">
-        <div className="flex justify-between align-top">
-        <h1 className="text-neutral-400 text-l font-semibold mb-2 md:w-full w-11/12">Thank you for beta testing! Here are recent changes and fixes.</h1>
-        <IoCloseOutline className="text-3xl text-red-800 btn-hover md:w-min w-1/12" onClick={()=>setVisible(false)}/>
-        </div>
-        <div className="mb-2">
+          <div className="flex justify-between align-top">
+            <h1 className="text-neutral-400 text-l font-semibold mb-2 md:w-full w-11/12">Thank you for beta testing! Here are recent changes and fixes.</h1>
+            <IoCloseOutline className="text-3xl text-red-800 btn-hover md:w-min w-1/12" onClick={() => setVisible(false)} />
+          </div>
+          <div className="mb-2">
             <span className="text-yellow-600 font-bold">In Development - </span>
             <span className="text-neutral-400">Getting cooked by my classes, updates on pause.</span>
-        </div>
-        <div className="max-h-24 overflow-y-auto">
+          </div>
+          <div className="max-h-24 overflow-y-auto">
+            <p className="text-neutral-400">🟩 2/23/2025 - Added pie chart analysis</p>
             <p className="text-neutral-400">🔨 2/18/2025 - Fixed task dragging on touchpad tap</p>
             <p className="text-neutral-400">🟩 2/18/2025 - Added task groups</p>
             <p className="text-neutral-400">🔃 2/7/2025 - UI changes</p>
@@ -44,10 +45,10 @@ export default function Changelog() {
             <p className="text-neutral-400">🟩 1/22/2025 - Added changelog page</p>
             <p className="text-neutral-400">🔨 1/20/2025 - Fixed null total time when completing a task</p>
             <p className="text-neutral-400">🔨 1/19/2025 - Fixed activity status not updating on desktop</p>
+          </div>
+          <h1 className="text-neutral-400 mt-2">🤍 Your feedback is valuable. If you encounter any bugs or ideas please reach out to me on discord @bagillionaire.</h1>
         </div>
-        <h1 className="text-neutral-400 mt-2">🤍 Your feedback is valuable. If you encounter any bugs or ideas please reach out to me on discord @bagillionaire.</h1>
-       </div> 
-        }
-        </>
-    )
+      }
+    </>
+  )
 }
