406dd719bacbc9f572a9a97d3e5a178578f8c9e3
harvset7777 <333harvest.moon333@gmail.com>
2025-03-14 16:18:50 -0700

    order

--- app/lockin/_components/LockedInTask.tsx
+++ app/lockin/_components/LockedInTask.tsx
@@ -23,12 +23,12 @@ const LockedInTask: React.FC<LockedInTaskProps> = ({
     <div className="w-full flex items-center align-middle justify-center space-x-5 p-4 rounded-xl outline outline-2 outline-appBg">
       {/* Button container */}
       <h1 className="font-semibold md:text-2xl text-xl text-center"> {startedFocusedTask ? "🔒" : "🔓"} {focusedTask?.name}</h1>
-      <IoCheckmarkOutline className="text-4xl btn-hover flex-none" onClick={() => handleCompleteTask(focusedTask)} />
       {!startedFocusedTask ? (
         <CiPlay1 className="text-4xl btn-hover flex-none" onClick={() => handleStartTask(focusedTask)} />
       ) : (
         <CiPause1 className="text-4xl btn-hover flex-none" onClick={() => handlePauseTask(focusedTask)} />
       )}
+      <IoCheckmarkOutline className="text-4xl btn-hover flex-none" onClick={() => handleCompleteTask(focusedTask)} />
     </div>
   );
 };
