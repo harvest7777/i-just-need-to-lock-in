a1e1d116f04fde869aad4675733c117b69fed8bb
harvset7777 <333harvest.moon333@gmail.com>
2025-03-26 23:04:52 -0700

    mobiel fix

--- app/profile/_components/ManageColors.tsx
+++ app/profile/_components/ManageColors.tsx
@@ -20,7 +20,7 @@ export default function ManageColors() {
   return (
     <div className="bg-app-fg rounded-2xl p-2 md:w-2/3 w-full flex flex-col space-y-4 pb-10 mt-5">
       <h1 className="text-center text-app-highlight font-bold text-3xl">Colors</h1>
-      <div className="w-full grid grid-cols-2 place-items-center items-start gap-x-2 gap-y-8">
+      <div className="w-full grid md:grid-cols-2 grid-cols-1 place-items-center items-start gap-x-2 gap-y-8">
         <button onClick={() => loadDefault()} className="btn-hover bg-app-bg rounded-xl" style={{ width: 200 }}>Load Defaults</button>
         <button onClick={() => loadDark()} className="btn-hover bg-app-bg rounded-xl" style={{ width: 200 }}>Load Dark</button>
         <ChangeColor displayName='Highlight' cssVariable='app-highlight' defaultColor='#009966' />
