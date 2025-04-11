20c77db7ea1d27787650cb4f1674059755e6d1c6
harvset7777 <333harvest.moon333@gmail.com>
2025-04-11 10:07:28 -0700

    remove dbg

--- app/lockin/_components/EnterPomodoroModal.tsx
+++ app/lockin/_components/EnterPomodoroModal.tsx
@@ -20,8 +20,8 @@ export default function EnterPomodoroModal({ setShowModal }: EnterPomodoroModalP
   const onSubmit = async (data: TimeFormInput) => {
     let pomodoroGoalMs: number = Number(data.minutesWork * 60 * 1000);
     let pomodoroBreak: number = Number(data.minutesBreak * 60 * 1000);
-    pomodoroGoalMs = 1000;
-    pomodoroBreak = 1000;
+    // pomodoroGoalMs = 1000;
+    // pomodoroBreak = 1000;
 
     console.log(data.notificationsEnabled)
     if (data.notificationsEnabled) {
